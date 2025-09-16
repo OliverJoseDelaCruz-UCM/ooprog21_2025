446003be753442975fe23f775565ae4c95d0b1d4
OliverJoseDelaCruz-UCM <oliverdelacruz2005@gmail.com>
2025-09-16 18:14:21 +0800

    Add files via upload

---
+++ Comments.java
@@ -0,0 +1,26 @@
+class Comments{
+
+    /**
+ * Comments.java
+ * This program demonstrates all three types of comments in Java:
+ * - Single-line
+ * - Multi-line
+ * - Documentation comments
+ */
+
+        // This is a single-line comment
+
+        /*
+         * This is a multi-line comment.
+         * It can span multiple lines.
+         * Useful for commenting out blocks of code or explanations.
+         */
+
+
+  /**
+         * This is a documentation comment.
+         * It is typically used for generating documentation using Javadoc.
+         * Here, it's just used as a standalone comment.
+         */
+
+}
